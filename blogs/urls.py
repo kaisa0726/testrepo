2328117d691dcb7b39ab10e4a3b63ae463e49bd7
hogehoge <test@ec.jp>
2020-11-21 12:30:58 +0900

    first_branch

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView,PostDetailView,CategoryListView,TagListView,CategoryPostView,TagPostView,SearchPostView,ReplayFormView,CommentFormView
+from .views import IndexView,PostDetailView,TagListView,CategoryListView,TagPostView,CategoryPostView
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -9,17 +9,10 @@ app_name = 'blogs'
 urlpatterns = [
     path('',IndexView.as_view(),name='index'),
     path('post/<int:pk>/',PostDetailView.as_view(),name='post_detail'),
-    path('categories/',CategoryListView.as_view(),name='category_list'),
-    path('tags/',TagListView.as_view(),name='tag_list'),
-    path('category/<str:category_slug>/',CategoryPostView.as_view(),name='category_post'),
-    path('tag/<str:tag_slug>/',TagPostView.as_view(),name='tag_post'),   
-    path('search/',SearchPostView.as_view(),name='search_post'),
-    path('comment/<int:pk>/',CommentFormView.as_view(),name='comment_form'),
-    path('comment/<int:pk>/approve/',views.comment_approve,name='comment_approve'),
-    path('commet/<int:pk>/remove/',views.comment_remove,name='comment_remove'),
-    path('replay/<int:pk>/',ReplayFormView.as_view(),name='replay_form'),
-    path('replay/<int:pk>/approve/',views.replay_approve,name='replay_approve'),
-    path('replay/<int:pk>/remove/',views.replay_remove,name='replay_remove'),
+    path('categories/',CategoryListView.as_view(),name='category'),
+    path('tags/',TagListView.as_view(),name='tags'),
+    path('tags/<str:category_slug>/',CategoryListView.as_view(),name='category_post'),
+    path('tag/<str:tag_slug>/',TagPostView.as_view(),name='tag_post'),
 ]
 
 if settings.DEBUG:
--- blogs/views.py
+++ blogs/views.py
@@ -9,29 +9,30 @@ from django.shortcuts import get_object_or_404,redirect
 from django.views.generic.edit import CreateView
 from .forms import CommentForm,ReplayForm
 from .models import Post,Category,Tag,Comment,Replay
-
 class PostDetailView(DetailView):
     model = Post
-    
-    def get_object(self,queryset=None):
+
+    def get_object(self, queryset=None):
         obj = super().get_object(queryset=queryset)
         if not obj.is_public and not self.request.user.is_authenticated:
             raise Http404
         return obj
-
-class IndexView(ListView):
-    model = Post
-    template_name = 'blogs/index.html'
-    context_object_name = 'index'
-    paginate_by = 1
-
+        
 class CategoryListView(ListView):
     queryset = Category.objects.annotate(
-        num_posts = Count('post',filter=Q(post__is_public=True)))
+        num_posts=Count('post',filter=Q(post__is_public=True)))
+        
 
 class TagListView(ListView):
     queryset = Tag.objects.annotate(
         num_posts = Count('post',filter=Q(post__is_public=True)))
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'blogs/index.html'
+    paginate_by = 2
+    contex_object_name = 'index'
+
 
 class CategoryPostView(ListView):
     model = Post
@@ -40,9 +41,9 @@ class CategoryPostView(ListView):
     def get_queryset(self):
         category_slug = self.kwargs['category_slug']
         self.category = get_object_or_404(Category,slug=category_slug)
-        qs = super().get_queryset.filter(category=self.category)
+        qs = super().get_queryset().filter(category=self.category)
         return qs
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["category"] = self.category
@@ -55,96 +56,11 @@ class TagPostView(ListView):
     def get_queryset(self):
         tag_slug = self.kwargs['tag_slug']
         self.tag = get_object_or_404(Tag,slug=tag_slug)
-        qs = super().get_queryset().filter(tags=self.tag)
+        qs = super().get_queryset().filter(Tag,slug=tag_slug)
         return qs
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["tag"] = self.tag
         return context
-    
-
-    
-class SearchPostView(ListView):
-    model =Post
-    template_name= 'blogs/search_post.html'
-    paginate_by = 3
-
-    def get_queryset(self):
-        query = self.request.GET.get('q',None)
-        lookups = (
-            Q(title__icontains=query)  |     
-            Q(text__icontains=query)    | 
-            Q(category__name__icontains=query)        |
-            Q(tags__name__icontains=query)       
-        )
-        if query is not None:
-            qs = super().get_queryset().filter(lookups).distinct()
-            return qs
-        qs = super().get_queryset()
-        return qs
-
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        query = self.request.GET.get('q')
-        context['query'] = query
-        return context
-    
-class CommentFormView(CreateView):
-    model = Comment
-    form_class = CommentForm
-
-    def form_valid(self,form):
-        comment = form.save(commit=False)
-        post_pk = self.kwargs['pk']
-        comment.post = get_object_or_404(Post,pk=post_pk)
-        comment.save()
-        return redirect('blogs:post_detail',pk=post_pk)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        post_pk = self.kwargs['pk']
-        context["post"] = get_object_or_404(Post,pk=post_pk)
-        return context
-
-@login_required
-def comment_approve(request,pk):
-    comment = get_object_or_404(Comment,pk=pk)
-    comment.approve()
-    return redirect('blogs:post_detail',pk=comment.post.pk)
-
-@login_required
-def comment_remove(request,pk):
-    comment =  get_object_or_404(Comment,pk=pk)
-    comment.delete()
-    return redirect('blogs:post_detail',pk=comment.post.pk)
-
-class ReplayFormView(CreateView):
-    model = Replay
-    form_class = ReplayForm
-
-    def form_valid(self,form):
-        replay = form.save(commit=False)
-        comment_pk = self.kwargs['pk']
-        replay.comment = get_object_or_404(Comment,pk=comment_pk)
-        replay.save()
-        return redirect('blogs:post_detail',pk=replay.comment.post.pk)
-
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        comment_pk = self.kwargs['pk']
-        context['comments'] = get_object_or_404(Comment,pk=comment_pk)
-        return context
-
-@login_required
-def replay_approve(request,pk):
-    replay = get_object_or_404(Replay,pk=pk)
-    replay.approve()
-    return redirect('blogs:post_detail',pk=replay.comment.post.pk)
-
-@login_required
-def replay_remove(request,pk):
-    replay = get_object_or_404(Replay,pk=pk)
-    replay.delete()
-    return redirect('blogs:post_detail',pk=replay.comment.post.pk)
     
